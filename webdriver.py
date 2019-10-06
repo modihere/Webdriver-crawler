c1b85b5b536a2e7b0739a5860da77a6e6bde2c60
Vivek Modi <nit.vivek@hotmail.com>
2019-10-06 17:02:05 +0530

    Imports restructured according to convention

--- webdriver.py
+++ webdriver.py
@@ -1,11 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 import re
 import os
 import time
 import json
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
